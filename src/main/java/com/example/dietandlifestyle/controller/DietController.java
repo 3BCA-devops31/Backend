4b7c9f2a7526e67d8faa7164415c0dd9618b87d6
kbhavani142005 <kbhavani142005@gamil.com>
2026-02-07 22:48:59 +0530

    frontend merge

--- src/main/java/com/example/dietandlifestyle/controller/DietController.java
+++ src/main/java/com/example/dietandlifestyle/controller/DietController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/diet")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(origins = "*")
 public class DietController {
     
     @Autowired
